b84f82deeb57a216fa84c7b1a0d0e9aae750618c
isaactekulu <isaactekulu@gmail.com>
2020-08-24 13:30:58 +1100

    Updates

---
+++ precache-manifest.37f785007233f3596392d3510f3c4c67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f828a94af1eb4aa12de34c70f32df51",
+    "url": "/monsters-inc/index.html"
+  },
+  {
+    "revision": "6b1928c395dde6a5d2f4",
+    "url": "/monsters-inc/static/css/main.4b8233d3.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/monsters-inc/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-inc/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b1928c395dde6a5d2f4",
+    "url": "/monsters-inc/static/js/main.f4c49563.chunk.js"
+  },
+  {
+    "revision": "c0386421228a86647961",
+    "url": "/monsters-inc/static/js/runtime-main.d7debb37.js"
+  }
+]);
